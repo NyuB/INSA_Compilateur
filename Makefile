e051279d9502c9e2356144ba8f0f44a02dd49380
NyuB <decaeste@etud.insa-toulouse.fr>
2020-03-20 14:25:32 +0100

    Test include

--- Makefile
+++ Makefile
@@ -12,8 +12,10 @@ comp : lex.yy.c y.tab.c
 lex.yy.c : lexer.l
 	${LEX} lexer.l
 
-y.tab.c : tokens.y
+y.tab.c : name_list.o tokens.y
 	${YACC} ${YACCFLAGS} tokens.y
+name_list.o : name_list.c name_list.h
+	${CC} ${CFLAGS} -c name_list.c
 
 
 
---
+++ name_list.h
@@ -0,0 +1,20 @@
+#ifndef H_NAME_LIST
+#define H_NAME_LIST
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define NOT_FOUND -1
+
+typedef struct name_list name_list;
+
+name_list * nli_empty(void);
+
+int nli_contains(name_list * list, char * name);
+
+void nli_append(name_list * list, char * name);
+
+void nli_display(name_list * list);
+
+void myncpy(char * dest,char * src,int n);
+
+#endif
